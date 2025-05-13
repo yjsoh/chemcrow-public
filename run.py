55ea4f0182d5e676b85eadc3102d4c9661441e75
Yuehua Xie <yux070@ucsd.edu>
2025-05-13 16:26:37 -0700

    modified run.py to be task 10

--- run.py
+++ run.py
@@ -6,4 +6,6 @@ chem_model = ChemCrow(
     tools_model="meta-llama/Meta-Llama-3.1-8B-Instruct",
     temp=0.1,
 )
-chem_model.run("What is the volatility of butadiene?")
+
+chem_model.run("Tell me what the boiling point is of the reaction product between isoamyl acetate and ethanol. To do this, predict the product of this reaction, and find its boiling point in celsius using cas number.")
+
